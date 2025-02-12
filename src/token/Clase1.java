300e6963d1e516897bad1c68d9afb25bb8bcee80
eric.dia <eric.dia@1DAW-16.mshome.net>
2025-02-12 10:32:50 +0100

    V2 ayuda jesusito

--- src/token/Clase1.java
+++ src/token/Clase1.java
@@ -14,7 +14,21 @@ public class Clase1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /*
+        1. Crear repositorio en GitHub
+        2. Copiar la url para indicarlo posteriormente en NetBeans
+        3. Crear token 
+        4. Inicializamos con git hub
+        5. Hacer un push
+        6. Insertar url y token 
+
+        ___________________________________________________________
+
+
+        7. Hacer un clone y ya estaría listo para modificar
+        8. Para guardar los cambios realizados hacer pull
+        */
+        
     }
     
 }
